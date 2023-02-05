b94306b261c7226ee40103c4ba8d5c0a42feb0d2
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-02-05 21:55:58 +0300

    feat: link projects and layouts

--- src/pages/boards/boards.ts
+++ src/pages/boards/boards.ts
@@ -1,10 +1,9 @@
 import getAsideHtml from '../home/getAsideHtml';
 import createNewBoard from '../../features/createNewBoard';
 import state from '../../state/state';
-import { signIn } from '../../API/users';
 import drawProjectsList from '../../features/drawProjectsList';
 import getColumnHTML from '../columns/columnsHtml';
-import { createColumns } from '../../API/columns';
+import { createColumns, getColumnsInBoard } from '../../API/columns';
 
 const Boards = {
   render: async () => `
@@ -14,18 +13,24 @@ const Boards = {
   </div>
   `,
   after_render: async () => {
-    //await signIn({ login: 'IMask', password: 'Tesla4ever' }); // LATER REMOVE THIS LINE
+    const array = window.location.hash.split('/').reverse().join('/');
+    const boardId = array.slice(0, array.indexOf('/'));
     const main = document.querySelector('.main-board');
+    const columns = await getColumnsInBoard(state.authToken, boardId);
+
     if (main) {
-      const firstColumn = await createColumns(state.authToken, state.boardId, { title: 'Todo', order: 0 });
-      const secondColumn = await createColumns(state.authToken, state.boardId, { title: 'In progress', order: 0 });
-      const thirdColumn = await createColumns(state.authToken, state.boardId, { title: 'Done', order: 0 });
-      const result = await getColumnHTML(state.authToken, state.boardId);
-      main.innerHTML = result;
+      if (columns.length !== 0) {
+        const result = await getColumnHTML(state.authToken, boardId);
+        main.innerHTML = result;
+      } else {
+        const firstColumn = await createColumns(state.authToken, state.boardId, { title: 'Todo', order: 0 });
+        const secondColumn = await createColumns(state.authToken, state.boardId, { title: 'In progress', order: 0 });
+        const thirdColumn = await createColumns(state.authToken, state.boardId, { title: 'Done', order: 0 });
+        const result = await getColumnHTML(state.authToken, state.boardId);
+        main.innerHTML = result;
+      }
     }
 
-    console.log(state.authToken);
-    console.log(state.boardId);
     const plusBtn = document.querySelector('.plus-img');
     if (plusBtn) {
       plusBtn.addEventListener('click', createNewBoard);
