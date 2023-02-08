81abb5345c0b557ca4415a6041efbd8319d3be88
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-02-08 21:15:30 +0300

    refactor: expand applying listen.ts

--- src/features/listen.ts
+++ src/features/listen.ts
@@ -3,7 +3,7 @@ import { FUNCTIONS } from '../constants/constants';
 const listen = (event: MouseEvent) => {
   const entries = Object.entries(FUNCTIONS);
   const target = event.target;
-  if (target && target instanceof HTMLButtonElement) {
+  if (target && target instanceof HTMLElement) {
     entries.forEach(async (el) => {
       if (target.classList.contains(el[0])) {
         await el[1]();
