882c2d6e285d4a9175b431958cbf8bc558fbddd3
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-02-09 11:06:36 +0300

    fix: remove magic numbers in getOppositeColor.ts

--- src/services/getOppositeColor.ts
+++ src/services/getOppositeColor.ts
@@ -1,9 +1,10 @@
 const getOppositeColor = (color: string) => {
   const MAX_COLOR = 'FFFFFF';
-  const maxColorDecimal = parseInt(MAX_COLOR, 16);
-  const colorDecimal = parseInt(color.slice(1), 16);
+  const SYSTEM_OF_NOTATION = 16;
+  const maxColorDecimal = parseInt(MAX_COLOR, SYSTEM_OF_NOTATION);
+  const colorDecimal = parseInt(color.slice(1), SYSTEM_OF_NOTATION);
   const oppositeDecimal = maxColorDecimal - colorDecimal;
-  return `#${oppositeDecimal.toString(16)}`;
+  return `#${oppositeDecimal.toString(SYSTEM_OF_NOTATION)}`;
 };
 
 export default getOppositeColor;
