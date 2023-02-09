258fa4151135a2717d44e8452d35b43602e8d86d
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-02-09 11:19:06 +0300

    refactor: rewrite getInactiveUsers with map

--- src/features/getInactiveUsers.ts
+++ src/features/getInactiveUsers.ts
@@ -2,11 +2,11 @@ import { User } from '../data/types';
 
 const getInactiveUsers = (users: User[], boardMembers: string[]) => {
   const result: User[] = [];
-  for (let i = 0; i < users.length; i += 1) {
-    if (!boardMembers.includes(users[i]._id)) {
-      result.push(users[i]);
+  users.map((el) => {
+    if (!boardMembers.includes(el._id)) {
+      result.push(el);
     }
-  }
+  });
 
   return result;
 };
