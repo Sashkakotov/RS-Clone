b990e3777e86876ee017aa1d2843bcb107ab1460
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-02-15 21:27:38 +0300

    feat: add user password change

--- src/features/changePassword.ts
+++ src/features/changePassword.ts
@@ -5,8 +5,9 @@ import state from '../state/state';
 const changePassword = async () => {
   const firstInput = document.querySelector('.new-password-input');
   const secondInput = document.querySelector('.repeat-new-password');
+  const regularExpression = /[0-9a-zA-Z!@#$%^&*]{6,}/;
   if (firstInput instanceof HTMLInputElement && secondInput instanceof HTMLInputElement) {
-    if (firstInput.value === secondInput.value) {
+    if (regularExpression.test(firstInput.value) && firstInput.value === secondInput.value) {
       const user: User = await getUserById(state.authToken, state.id);
       await updateUser(state.authToken, state.id, { name: user.name, login: user.login, password: secondInput.value });
       state.password = secondInput.value;
