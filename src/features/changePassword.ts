0bca3327b9408a7d4106e18ce2e2911af8a9add1
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-02-15 21:48:54 +0300

    feat: add pop up messages for wrong changes

--- src/features/changePassword.ts
+++ src/features/changePassword.ts
@@ -1,13 +1,19 @@
 import { getUserById, updateUser } from '../API/users';
-import { User } from '../data/types';
+import { User, ToastrType } from '../data/types';
 import state from '../state/state';
+import popUpMessages from './popUpMessages/popupMessages';
+import UI from '../data/UI';
 
 const changePassword = async () => {
   const firstInput = document.querySelector('.new-password-input');
   const secondInput = document.querySelector('.repeat-new-password');
   const regularExpression = /[0-9a-zA-Z!@#$%^&*]{6,}/;
   if (firstInput instanceof HTMLInputElement && secondInput instanceof HTMLInputElement) {
-    if (regularExpression.test(firstInput.value) && firstInput.value === secondInput.value) {
+    if (!regularExpression.test(firstInput.value)) {
+      popUpMessages(ToastrType.error, UI.wrongPasswordMessage);
+    } else if (firstInput.value !== secondInput.value) {
+      popUpMessages(ToastrType.error, UI.wrongPasswordRepeatMessage);
+    } else {
       const user: User = await getUserById(state.authToken, state.id);
       await updateUser(state.authToken, state.id, { name: user.name, login: user.login, password: secondInput.value });
       state.password = secondInput.value;
