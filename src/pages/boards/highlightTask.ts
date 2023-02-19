ff5ef2374bc1dae886275a12458c3e4b0fe0e9d8
katsiaryna-andrabaila <androbailoe@gmail.com>
2023-02-19 14:58:25 +0300

    refactor: remove unnecessary code

--- src/pages/boards/highlightTask.ts
+++ src/pages/boards/highlightTask.ts
@@ -1,14 +1,8 @@
 import state from '../../state/state';
-import { getTasksSetByBoardId } from '../../API/tasks';
-import { ITasks } from '../../data/types';
-import getBoardId from '../../services/getBoardId';
 import { DELAY } from '../../constants/constants';
 
 const highlightTask = async () => {
-  const boardId = getBoardId();
-  const tasksInBoard: ITasks[] = await getTasksSetByBoardId(state.authToken, boardId);
-  const targetTask = tasksInBoard.filter((el) => el._id === state.selectedTask);
-  const targetTaskElement = document.getElementById(targetTask[0]._id);
+  const targetTaskElement = document.getElementById(state.selectedTask);
   targetTaskElement?.setAttribute('name', 'target');
 
   targetTaskElement?.classList.add('highlighted');
